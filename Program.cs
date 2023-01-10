54253c86a43f4807ad8910f12f0979b4ff6447d7
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-10 23:42:31 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quantity_Of_Symbols
+{
+    class Program
+    {
+        static void PrintLine(string symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the Symbol");
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine("Write the Quantity of Simbols");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+            PrintLine(symbol, symbolsCount);
+            Console.ReadLine();
+        }
+    }
+}
